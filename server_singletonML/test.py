3117915055799abb265834dbd6986ff761a3268c
chj4535 <chj4535@ajou.ac.kr>
2019-10-18 14:30:06 +0900

    fasttext 서버용 singleton 적용

---
+++ server_singletonML/test.py
@@ -0,0 +1,4 @@
+import singleton_fasttext
+
+FT = singleton_fasttext.singleton_fasttext()
+FT.makeSentence("시계",0.1,0.9)
--- src/fasttext_evali.py
+++ src/fasttext_evali.py
@@ -1,3 +1,5 @@
+# %%
+
 from gensim.models import fasttext as fText
 from sklearn.manifold import TSNE
 import matplotlib.pylab as plt
@@ -6,34 +8,84 @@ import numpy as np
 from matplotlib import font_manager, rc
 import fasttext as ft
 
-font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
-rc('font', family=font_name)
+model = None
+vocab_list = None
+adj_list = None
 
-model = ft.load_model('./fasttext/fasttext.bin');
-model2 = fText.load_facebook_model('./fasttext/fasttext.bin')
+model = fText.load_facebook_model('./fasttext/fasttext.bin')
 print("load_model_success!!")
-
-# print(model.get_nearest_neighbors('핸드폰'))
-# print(model2.(positive=['소리','물건']))
-# model2.wv.similar_by_word('소리',topn=100)
-# model2.wv.most_similar(positive=['소리','물건'])
-
 vocab_list = open("label_list.txt", 'r', encoding="utf-8").read().split()
-for vocab_item in vocab_list:
-    print(vocab_item)
-# f.close()
-print(len(vocab_list))
+adj_list = open("adj_list.txt", 'r', encoding="utf-8").read().split()
 
-sm_A_list=[]
-sm_B_list =[]
-sm_AB_list =[]
-k1 = '컴퓨터'
-k2 = '카메라'
-for vocab_item in vocab_list:
-    sm_A_list.append([vocab_item,model2.similarity(vocab_item,k1)])
-    sm_B_list.append([vocab_item, model2.similarity(vocab_item, k2)])
-    sm_AB_list.append([vocab_item,(sm_A_list[-1][1]*0.7+sm_B_list[-1][1]*0.3)])
+# %%
 
-sm_AB_list = sorted(sm_AB_list,key=lambda acc:acc[1],reverse=True)
-print(sm_AB_list)
-model.get_analogies("시계","효과")
+import secrets
+
+
+def makeSentence(k1, min, max):
+    sm_A_list = []
+    for vocab_item in vocab_list:
+        vocab = model.similarity(vocab_item, k1)
+        if vocab > min and vocab < max:
+            sm_A_list.append([vocab_item, vocab])
+    sm_A_list = sorted(sm_A_list, key=lambda acc: acc[1], reverse=True)
+    #     print(sm_A_list)
+
+    select_label = secrets.choice(sm_A_list)
+    print("select_label : ", select_label)
+
+    adj_A_list = []
+    adj_B_list = []
+    for adj_item in adj_list:
+        adj_A = model.similarity(adj_item, k1)
+        adj_B = model.similarity(adj_item, select_label[0])
+        adj_A_list.append([k1, select_label[0], adj_item, adj_A * adj_B])
+    adj_A_list = sorted(adj_A_list, key=lambda acc: acc[3], reverse=True)
+    #     print(adj_A_list)
+    for sentence in adj_A_list:
+        print(sentence[0] + ' ' + sentence[2] + ' ' + sentence[1])
+
+
+# def main():
+#     k1 = '안경에'
+#     k2 = '안경에'
+#     makeSentence(k1,0.3,0.5)
+
+# main()
+makeSentence(k1, 0.3, 0.5)
+
+# %%
+
+import secrets
+
+
+def makeSentence2(k1, k2, min, max):
+    sm_A_list = []
+    for vocab_item in vocab_list:
+        vocab1 = model.similarity(vocab_item, k1)
+        vocab2 = model.similarity(vocab_item, k2)
+        #         if vocab1>min and vocab1<max and vocab2>min and vocab2<max:
+        sm_A_list.append([vocab_item, vocab1 * (1 - vocab2)])
+    sm_A_list = sorted(sm_A_list, key=lambda acc: acc[1], reverse=True)
+    print(sm_A_list)
+    select_label = secrets.choice(sm_A_list)
+    #     for item in sm_A_list:
+    #         if item[0]=='신호등':
+    #             select_label = item
+
+    k3 = k1 + " " + k2
+    adj_A_list = []
+    adj_B_list = []
+    for adj_item in adj_list:
+        adj_A = model.similarity(adj_item, k3)
+        adj_B = model.similarity(adj_item, select_label[0])
+        adj_A_list.append([k3, select_label[0], adj_item, adj_A * adj_B])
+    adj_A_list = sorted(adj_A_list, key=lambda acc: acc[3], reverse=True)
+    #     print(adj_A_list)
+    for sentence in adj_A_list:
+        print(sentence[0] + ' ' + sentence[2] + ' ' + sentence[1])
+
+
+k1 = '초콜릿을'
+k2 = '발렌타인데이에'
+makeSentence2(k1, k2, 0.3, 0.5)
